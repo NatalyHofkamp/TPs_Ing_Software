05ade6e3824fe3ff41797c82f79839b4c6ca2bbf
luciafernandezvincent <lfernandezvincent@udesa.edu.ar>
2025-03-13 15:29:19 -0300

    primer try

--- tp1/Route.hs
+++ tp1/Route.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Use newtype instead of data" #-}
 
 module Route ( Route, newR, inOrderR )
   where 
@@ -7,13 +9,21 @@ data Route = Rou [ String ] deriving (Eq, Show)
 newR :: [ String ] -> Route                    -- construye una ruta segun una lista de ciudades
 newR cities = Rou cities
 
-findIndex :: Route -> String -> Int 
-findIndex i1 | i1<-[0..length(route)] , (!!) route i1 = city1 
-             |  otherwise = -1 
+-- findIndex :: Route -> String -> Int 
+-- findIndex i1 | i1<-[0..length(route)] , (!!) route i1 = city1 
+--              |  otherwise = -1 
+
+
+-- inOrderR :: Route -> String -> String -> Bool
+-- inOrderR route city1 city2 = i1 /= -1 && i2 /= -1 && i1 < i2
+--   where
+--     i1 = findIndex route city1
+--     i2 = findIndex route city2
 
 
 inOrderR :: Route -> String -> String -> Bool
-inOrderR route city1 city2 = i1 /= -1 && i2 /= -1 && i1 < i2
-  where
-    i1 = findIndex route city1
-    i2 = findIndex route city2
+inOrderR (Rou (c:cs)) city1 city2
+    | c == city1 = city2 `elem` cs  -- Si encontramos city1, verificamos si city2 está después en la lista
+    | c == city2 = False            -- Si encontramos city2 antes que city1, retornamos False
+    | otherwise = inOrderR (Rou cs) city1 city2  -- Seguimos buscando en la lista
+inOrderR _ _ _ = False  -- Caso base: lista vacía, retorna False
---
+++ tp1/Stack.hs
@@ -0,0 +1,45 @@
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
+module Stack ( Stack, newS, freeCellsS, stackS, netS, holdsS, popS )
+  where
+
+import Palet
+import Route
+
+data Stack = Sta [ Palet ] Int deriving (Eq, Show)
+
+createpalets :: Int -> [Palet]
+createpalets x = replicate x (newP "" 0)
+
+newS :: Int -> Stack                      -- construye una Pila con la capacidad indicada 
+newS capacity = Sta [] capacity
+
+freeCellsS :: Stack -> Int                -- responde la celdas disponibles en la pila
+freeCellsS (Sta palets _) = length (filter (\palet -> netP palet == 0) palets)
+
+stackS :: Stack -> Palet -> Stack         -- apila el palet indicado en la pila
+stackS (Sta palets capacity) palet
+  | length palets < capacity = Sta (palet : palets) capacity -- CUBRIR TEMA PESO
+  | otherwise = error "Stack is full"
+
+netS :: Stack -> Int                      -- responde el peso neto de los paletes en la pila
+netS (Sta palets _) = sum (map netP palets)
+
+holdsS :: Stack -> Palet -> Route -> Bool
+holdsS stack new_palet route =
+    freeCellsS stack > 0 && inOrderR route (destinationP new_palet) (destinationP (lastPalet stack))
+
+-- Función auxiliar para obtener el último palet en la pila (el de arriba)
+lastPalet :: Stack -> Palet
+lastPalet (Sta [] _) = error "Stack is empty"
+lastPalet (Sta palets _) = last palets
+
+popS :: Stack -> String -> Stack
+popS (Sta palets capacity) city = Sta (filter (\palet -> destinationP palet /= city) palets) capacity
+   -- quita del tope los paletes con destino en la ciudad indicada
+
+{-
+TESTEO
+PONER MAS PALETS D LA CANT DISPONIBLES (CANTIDAD)
+PONER MAS PESO DEL QUE HAY
+-}
