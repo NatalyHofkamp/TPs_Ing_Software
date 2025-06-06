3d9264c551c2a392275d50449cb1711e5c805d12
luciafernandezvincent <lfernandezvincent@udesa.edu.ar>
2025-06-06 12:49:24 -0300

    yes

--- tp4/src/main/java/org/udesa/tp4/controller/UnoController.java
+++ tp4/src/main/java/org/udesa/tp4/controller/UnoController.java
@@ -1,12 +1,29 @@
 package org.udesa.tp4.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class UnoController {
-    @GetMapping ("/")
-    public String saludo() {return "index";}
+//    @GetMapping ("/")
+//    public String saludo() {return "index";}
 //    public String saludo() {return "hola luidolacrack";}
+
+    @GetMapping("/hola")
+    public ResponseEntity<String> holaMundo() {
+        return new ResponseEntity<>("respuesta a Hola Mundo", HttpStatus.OK);
+    }
+    @PostMapping("newmatch") public ResponseEntity newMatch( @RequestParam List<String> players ) {return ResponseEntity.ok("codigoooooo");}
+//    @PostMapping("play/{matchId}/{player}") public ResponseEntity play( @PathVariable UUID matchId, @PathVariable String player, @RequestBody JsonCard card ) {}
+//    @PostMapping("draw/{matchId}/{player}") public ResponseEntity drawCard( @PathVariable UUID matchId, @RequestParam String player ) {}
+//    @GetMapping("activecard/{matchId}") public ResponseEntity activeCard( @PathVariable UUID matchId ) {}
+//    @GetMapping("playerhand/{matchId}") public ResponseEntity playerHand( @PathVariable UUID matchId ) {}
 
 }
